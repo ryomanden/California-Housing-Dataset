9a54bffd9c091d36822777876b38249123858fdd
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2024-06-06 02:07:37 +0900

    add comment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+# Load the data
 header = [
     'longitude',
     'latitude',
@@ -15,10 +16,11 @@ header = [
 ]
 data = pd.read_csv('CaliforniaHousing/cal_housing.data', header=None, names=header)
 
+# Correlation matrix
 corr = data.corr()
 
+# Plotting the heatmap
 plt.figure()
 sns.heatmap(corr, annot=True)
-
 plt.savefig('result/corr_heatmap.png')
 plt.show()
