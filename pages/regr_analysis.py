c5b7401e97e5d769363669d4d744dedd7013770f
Ryo Mitsuda <s2132134lj@s.chibakoudai.jp>
2024-06-10 17:19:10 +0900

    add z_score and fix layout

--- pages/regr_analysis.py
+++ pages/regr_analysis.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 from main import get_divided_data
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+from scipy import stats as status
 import streamlit as st
 import matplotlib.pyplot as plt
 
@@ -10,10 +13,17 @@ model_lr = None
 
 
 # --- 単回帰分析 --- #
-def single_regr_analysis(object_val: str, explanatory_val: str):
+def single_regr_analysis(object_val: str, explanatory_val: str, z_score_threshold=3.0, sel_delete ='delete'):
     # 単回帰分析
-    x = data[[object_val]]
-    y = data[[explanatory_val]]
+
+    z_score = np.abs(status.zscore(data))
+    if sel_delete == 'delete':
+        cleaned_data = data[(z_score < z_score_threshold).all(axis=1)]
+    else:
+        cleaned_data = data
+
+    x = cleaned_data[[object_val]]
+    y = cleaned_data[[explanatory_val]]
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
@@ -40,20 +50,29 @@ def single_regr_analysis(object_val: str, explanatory_val: str):
     # 結果の表示
     st.pyplot(plt)
 
-    st.write(f'平均二乗誤差: {mse}')
-    st.write(f'決定係数: {r2}')
+    col1, col2 = st.columns(2)
+    col1.metric('平均二乗誤差', mse)
+    col2.metric('決定係数', r2)
 
     # 任意値の予測
-    x_pre = st.number_input('独立変数の値', min_value=0.0, max_value=100.0, value=0.0, step=0.1)
+    x_pre = col1.number_input('独立変数の値', min_value=0.0, max_value=100.0, value=0.0, step=0.1)
     y_pre = model_lr.predict([[x_pre]])
 
-    st.write(f'{explanatory_val} の予測値は {y_pre[0][0]}')
+    col2.metric(f'{explanatory_val} の予測値',y_pre[0][0])
+
 
 # --- 重回帰分析 --- #
-def multi_regr_analysis():
+def multi_regr_analysis(z_score_threshold=3.0, sel_delete='delete'):
+
+    z_score = np.abs(status.zscore(data))
+    if sel_delete == 'delete':
+        cleaned_data = data[(z_score < z_score_threshold).all(axis=1)]
+    else:
+        cleaned_data = data
+
     # 重回帰分析
-    x = data[['housingMedianAge', 'totalRooms', 'totalBedrooms', 'population', 'medianIncome']]
-    y = data[['medianHouseValue']]
+    x = cleaned_data[['housingMedianAge', 'totalRooms', 'totalBedrooms', 'population', 'medianIncome']]
+    y = cleaned_data[['medianHouseValue']]
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
 
     # モデルの学習
@@ -71,9 +90,10 @@ def multi_regr_analysis():
     # 決定係数 (R^2) の計算
     r2 = r2_score(y_test, y_pred)
 
-    # 結果の表示
-    st.write(f'平均二乗誤差: {mse}')
-    st.write(f'決定係数: {r2}')
+    # 結果の表示\
+    col1, col2 = st.columns(2)
+    col1.metric('平均二乗誤差', mse)
+    col2.metric('決定係数', r2)
 
     # 任意値の予測
     housingMedianAge = st.number_input('housingMedianAge', min_value=0.0, max_value=100.0, value=0.0, step=0.1)
@@ -89,17 +109,30 @@ def multi_regr_analysis():
 def regr_analysis():
     with st.form('single_regr_analysis'):
         st.title('単回帰分析')
-        object_val = st.selectbox('説明変数の選択', data.columns[2:])
-        explanatory_val = st.selectbox('目的変数の選択', data.columns[2:])
+
+        col1, col2 = st.columns(2)
+        object_val = col1.selectbox('説明変数の選択', data.columns[2:])
+        explanatory_val = col2.selectbox('目的変数の選択', data.columns[2:])
+
+        sel_delete = col1.radio('外れ値の処理', ('delete', 'keep'))
+        z_score_threshold = col2.number_input('Z-scoreの閾値', min_value=0.0, max_value=10.0, value=3.0, step=0.1)
+
         submit_button = st.form_submit_button(label='Submit')
+
         if submit_button:
-            single_regr_analysis(object_val, explanatory_val)
+            single_regr_analysis(object_val, explanatory_val, z_score_threshold, sel_delete)
 
     with st.form('multi_regr_analysis'):
         st.title('重回帰分析')
+
+        col1, col2 = st.columns(2)
+        sel_delete = col1.radio('外れ値の処理', ('delete', 'keep'))
+        z_score_threshold = col2.number_input('Z-scoreの閾値', min_value=0.0, max_value=10.0, value=3.0, step=0.1)
+
         submit_button = st.form_submit_button(label='Submit')
+
         if submit_button:
-            multi_regr_analysis()
+            multi_regr_analysis(z_score_threshold, sel_delete)
 
 
 if __name__ == '__main__':
